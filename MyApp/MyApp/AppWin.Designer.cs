e6d08911622bc1dc4c816562a63127e32bb0bffa
redarcher69 <alwxvidami@gmail.com>
2025-03-15 02:09:14 +0200

    FirstProjectIteration
    From settings as : position , size , Z-poze and a close button with a  image hover change

---
+++ MyApp/MyApp/AppWin.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MyApp
+{
+    partial class AppWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.BackColor = System.Drawing.Color.DarkSalmon;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Image = global::MyApp.Properties.Resources.SUschi;
+            this.closeButton.Location = new System.Drawing.Point(550, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(32, 32);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.closeButton.MouseLeave += new System.EventHandler(this.closeButton_MouseLeave);
+            this.closeButton.MouseEnter += new System.EventHandler(this.closeButton_MouseEnter);
+            // 
+            // AppWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = global::MyApp.Properties.Resources.Screenshot_20250214_232908;
+            this.ClientSize = new System.Drawing.Size(582, 153);
+            this.Controls.Add(this.closeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "AppWin";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ApplicationWindow";
+            this.Load += new System.EventHandler(this.AppWin_Load);
+            this.Shown += new System.EventHandler(this.AppWin_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ MyApp/MyApp/AppWin.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class AppWin : Form
+    {
+        #region user32.dll Constants
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+        private const int SM_CXSCREEN = 0;
+        private const int SM_CYSCREEN = 1;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOACTIVATE = 0x0010;
+
+        private const int WM_MOUSEACTIVATE = 0x0021;
+        private const int MA_NOACTIVATE = 3;
+        #endregion
+
+        #region WinForm Size Constants  
+        private const int heightResolutionFrom = 600;
+        private const int heightResolutionTo = 200;
+        #endregion
+
+        public AppWin()
+        {
+            InitializeComponent();
+            InitializeCloseButton();
+        }
+
+        private void AppWin_Load(object sender, EventArgs e)
+        {
+            SetLocationAndSize();
+        }
+
+        private void SetLocationAndSize()
+        {
+            //Form location
+            int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+            Location = new Point(screenWidth - heightResolutionFrom, 0);
+
+            //Form size
+            Size = new Size(heightResolutionFrom, heightResolutionTo);
+            AutoSize = false;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private void AppWin_Shown(object sender, EventArgs e)
+        {
+            //Set form to bottom
+            SetWindowPos(this.Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_MOUSEACTIVATE)
+            {
+                m.Result = (IntPtr)MA_NOACTIVATE;
+                return;
+            }
+            base.WndProc(ref m);
+        }
+
+        #region Button Events
+        private void InitializeCloseButton()
+        {
+     
+        }
+
+        
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void closeButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.Image = Properties.Resources.download__4_;
+        }
+
+        private void closeButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.Image = Properties.Resources.SUschi;
+        }
+        #endregion
+
+    }
+}
